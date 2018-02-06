4d5cd5e9d31416745be1ee974fbd1c44dbe8d5b2
JPierce95 <jpierce18@mail.csuchico.edu>
2018-02-05 17:22:58 -0800

    I also added an else if the numbers are equal.

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,13 +5,21 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+        int num1, num2, biggestNum;
+        cout<<"Please enter a whole number:\n";
+        cin>> num1;
+        cout<<"Please enter another whole number:\n";
+        cin>> num2;
+        if(num1>num2){
+                cout<<"Of those two numbers, the biggest is: "<< num2 << "!\n";
+        }
+        if(num1 < num2){
+                cout<<"Of those two numbers, the biggest is: " << num2 << "!\n";
+        }
+        else{
+                cout<<"These two numbers are equal!\n";
+        }
+        cout<<"Thank you for playing.\n";
 
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
-
-  return 0;
+        return 0;
 }
